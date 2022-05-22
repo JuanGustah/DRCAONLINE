3b399c3511986934d74cd3ee67eeb2cb1cfde097
JuanGustah <juangt360graus@gmail.com>
2022-05-22 02:37:31 -0300

    fix url fetch

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ window.onload=async function(){
 
     const timeoutId = setTimeout(() => controller.abort(), 5000);
 
-    await fetch('https://drca.ufrpe.br',{
+    await fetch('http://drca.ufrpe.br',{
         method:'GET',
         signal:controller.signal
     }).then(()=>{
