bcfc911c0b98af0883e80a0e9237d799b1d3c6fe
JuanGustah <juangt360graus@gmail.com>
2022-05-22 01:32:01 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,39 @@
+window.onload=async function(){
+    const controller = new AbortController();
+
+    const timeoutId = setTimeout(() => controller.abort(), 5000);
+
+    await fetch('http://drca.ufrpe.br',{
+        method:'GET',
+        signal:controller.signal
+    }).then(()=>{
+        let loadingHtml= document.querySelector('.loading-container');
+        loadingHtml.style.display="none";
+
+        let responseHtml= document.querySelector('.response');
+        responseHtml.style.display="flex";
+        responseHtml.style.backgroundColor="#2ca328";
+
+        let responseText=document.createTextNode("Sim, está disponível. \u{1F64F}")
+        responseHtml.appendChild(responseText)
+
+        let explicationHtml= document.querySelector('.explication');
+        let explicationText=document.createTextNode("Se você ainda está enfrentando problemas para solicitar sua confirmação entre em contato com o DRCA por email ou espere alguns minutos para tentar novamente!")
+        explicationHtml.appendChild(explicationText)
+    })
+    .catch(function(err) { 
+        let loadingHtml= document.querySelector('.loading-container');
+        loadingHtml.style.display="none";
+
+        let responseHtml= document.querySelector('.response');
+        responseHtml.style.display="flex";
+        responseHtml.style.backgroundColor="#b11a1a";
+
+        let responseText=document.createTextNode("Não, está fora do ar. \u{1F641}")
+        responseHtml.appendChild(responseText)
+
+        let explicationHtml= document.querySelector('.explication');
+        let explicationText=document.createTextNode("Não, não é seu PC que está com problema. O site do DRCA está fora do ar. Mas não se preocupe! Em breve o site voltará ao ar...")
+        explicationHtml.appendChild(explicationText)
+    });
+}
